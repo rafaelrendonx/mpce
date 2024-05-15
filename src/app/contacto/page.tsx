1f26ffac39d023a1ea188dd39ba3a3e76141f853
rafaelrendonx <rafael_x@hotmail.com>
2024-05-14 18:41:52 -0700

    Se modifica sección de Contacto como prueba del server de desarrollo

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -13,7 +13,7 @@ function Contacto() {
             <div className="bg-[url('https://source.unsplash.com/random/?Construction&12')] min-h-[25vh] bg-no-repeat bg-cover flex items-center justify-center">
                 <div className='flex min-h-[25vh] items-center justify-center pt-10'>
                     <div className='text-center font-light text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] sm:font-normal lg:py-8 lg:px-40 lg:font-light'>
-                        <h1 className='text-5xl'>Contacto</h1>
+                        <h1 className='text-5xl'>Contactox</h1>
                     </div>
                 </div>
             </div>
