c78f30707add3fc4964b534d1857d8edb54bdc76
rafaelrendonx <rafael_x@hotmail.com>
2024-05-15 18:13:20 -0700

    Removed Contact from Home

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,18 +3,15 @@ import Image from 'next/image';
 
 import { FacebookIcon, InstagramIcon, MapPin } from 'lucide-react';
 
-import { mpceAddress } from '@/lib/constants';
-
-import logo from '@/../public/images/logos/mpce-siglas.svg';
 import whatsappIcon from '@/../public/whatsapp-white-icon.svg';
 
 export const Footer = () => {
 	return (
 
 		<footer className='bg-mbceYellow/90 flex min-h-[10vh] items-center relative'>
-			
+
 			<div className='absolute w-full z-10'>
-				
+
 				<div className='flex items-center lg:container lg:pl-4 lg:pr-4 xl:pl-4 xl:pr-4 2xl:pl-0 2xl:pr-0'>
 
 					{/* Copy right */}
@@ -24,7 +21,7 @@ export const Footer = () => {
 						</p>
 					</div>
 
-					{/* Socials */} 
+					{/* Socials */}
 					<div className='flex flex-col basis-1/2 pr-5 2xl:pr-0'>
 						<ul className='text-white flex items-center justify-end gap-5'>
 							<li>
@@ -38,7 +35,7 @@ export const Footer = () => {
 								</Link>
 							</li>
 							<li>
-								<Link className='flex items-center' href={'#'} >
+								<Link className='flex items-center' href={`https://wa.me/+526624669086?text=¡Hola!+Me+gustaría+información+de+un+servicio`} >
 									<Image width={25} height={25} src={whatsappIcon.src} alt='whatsapp'
 									/>
 								</Link>
